a7837a05566311f5a14dc2a430cf9125c8f145ad
luisEfe80 <luismendez80@hotmail.com>
2021-05-22 10:06:55 -0400

    content adjustment

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -19,9 +19,98 @@ section
   .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
     .titulo__template--a.mb-4
       span.h6 3. Competencias a desarrollar
-    p.mb-3 Para responder a las dinámicas de los procesos inherentes al mercadeo, el SENA oferta, la Tecnología en Desarrollo de Procesos de Mercadeo, con la finalidad de que, las acciones de marketing aplicadas por las organizaciones se conviertan en una actividad empresarial proactiva, inteligente y dinámica; en donde el empresario, debe romper paradigmas para aceptar que esta área, impacte las demás áreas funcionales de la empresa y se convierta, en el “alma de la organización” (Herrera, 2013).
-    p.mb-3 Teniendo en cuenta, que el mercadeo, “es el proceso de desarrollar, promover y distribuir productos, con el fin de responder a las exigencias y deseos de los clientes”(Louis Scheeider  Farese, 2001); es innegable, la necesidad de que toda organización  comercial, cuente con, Asistentes de mercadeo, publicidad y comunicaciones, que intervengan en el desarrollo de estrategias de mercadeo; premisa  con  la  cual,  parecen  estar  de acuerdo los empresarios; ya que las ocupaciones relacionadas con mercadeo y ventas reportan un mayor número de vacantes colocados y está dentro de las diez principales ocupaciones, de la mesa sectorial (APE -SENA, 2017).
-    p Finalmente, las oportunidades de vinculación laboral para los Asistentes de mercadeo, publicidad y comunicaciones, son prometedoras, si se tiene en cuenta, el incremento en la demanda de personal capacitado, en ocupaciones relacionadas con los servicios; la cual representó, el 55% del total de vacantes registradas en el Servicio público de empleo del SENA –APE, en el año 2017, que si se compara, con la cifra del año inmediatamente anterior, se observó una variación positiva del 4,01%  (Observatorio SPE -SENA, 2017).
+    .tabla-b.color-acento-contenido.mb-5
+      .tabla-b__header
+        .h6.mb-0 Competencias Técnicas o Específicas
+      table
+        tr
+          th Norma Sectorial de Competencia Laboral
+          th Código NSCL
+        tr  
+          td Formular el plan de mercadeo según comportamiento del mercado y metodología.
+          td 260101061
+        tr  
+          td Definir el plan de comunicación interna de acuerdo con la cultura y estrategia corporativa.
+          td 260101053
+        tr  
+          td Definir el plan de Merchandising según objetivos y técnicas de la promoción.
+          td 260101050
+        tr  
+          td Definir el plan de Merchandising según objetivos y técnicas de la promoción.
+          td 260101059
+        tr  
+          td Evaluar estrategias de mercadeo de acuerdo con objetivos comerciales.
+          td 260101009
+
+    .tabla-b.color-acento-contenido.mb-5
+      .tabla-b__header
+        .h6.mb-0 Competencia Inducción
+      table
+        tr
+          th Norma Sectorial de Competencia Laboral
+          th Código NSCL
+        tr  
+          td Resultado de Aprendizaje de la Inducción-Inducción
+          td 240201530
+    .tabla-b.color-acento-contenido.mb-5
+      .tabla-b__header
+        .h6.mb-0 Competencias Transversales
+      table
+        tr
+          th Norma Sectorial de Competencia Laboral
+          th Código UC
+        tr  
+          td Generar hábitos saludables de vida mediante la aplicación de programas de actividad física en los contextos productivos y sociales –
+            span.text-bold Actividad física y hábitos de vida saludable.
+          td 230101507
+        tr  
+          td Gestionar procesos propios de la cultura emprendedora y empresarial de acuerdo con el perfil personal y los requerimientos productivo y social –
+            span.text-bold Cultura emprendedora y empresarial.
+          td 240201529
+        tr  
+          td Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo de acuerdo con las políticas organizacionales y la normatividad vigente –
+            span.text-bold Protección para la salud y el medio ambiente.
+          td 220601501        
+        tr  
+          td Enrique Low Murtra -Interactuar en el contexto productivo y social de acuerdo con principios éticos para la construcción de una cultura de paz –
+            span.text-bold Ética para la construcción de una cultura de paz.
+          td 240201526
+        tr  
+          td Ejercer derechos fundamentales del trabajo en el marco de la constitución política y los convenios internacionales –
+            span.text-bold Derechos fundamentales del trabajo.
+          td 210201501
+        tr  
+          td Orientar investigación formativa según referentes técnicos –
+            span.text-bold Investigación.
+          td 240201064
+    .tabla-b.color-acento-contenido.mb-5
+      .tabla-b__header
+        .h6.mb-0 Competencias Clave
+      table
+        tr
+          th Norma Sectorial de Competencia Laboral
+          th Código UC
+        tr  
+          td Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales. –
+            span.text-bold Matemática.
+          td 240201528
+        tr  
+          td Aplicación de conocimientos de las ciencias naturales de acuerdo con situaciones del contexto productivo y social –
+            span.text-bold Ciencias Naturales: Física.
+          td 220201501
+        tr  
+          td Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de información –
+            span.text-bold TIC.
+          td 220501046      
+        tr  
+          td Desarrollar procesos de comunicación eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal y productivo –
+            span.text-bold Comunicación.
+          td 240201524
+        tr  
+          td Interactuar en lengua inglesa de forma oral y escrita dentro de contextos sociales y laborales según los criterios establecidos por el Marco Común Europeo de referencia para las lenguas–
+            span.text-bold Inglés.
+          td 240202501
+       
 
   .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
     .titulo__template--a.mb-4
