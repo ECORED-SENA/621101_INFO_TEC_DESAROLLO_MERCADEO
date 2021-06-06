218b6479e1616d06415f49e9763bc3fccfdfc8fd
luisEfe80 <luismendez80@hotmail.com>
2021-06-05 23:50:09 -0400

    Actualización del video de presentación

--- src/config/global.js
+++ src/config/global.js
@@ -18,7 +18,7 @@ export const global = {
   ],
   //- imagenBannerPrincipal: require('@/assets/curso/banner-princiapal.svg'),
   videoBannerPrincipal:
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/gxSx_yCHdgQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/l82vG7Rbbno" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
   fondoBannerPrincipal: require('@/assets/curso/banner-principal.svg'),
 }
 export const menuPrincipal = {
